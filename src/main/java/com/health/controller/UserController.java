b14c32d0c9c2aa77ab0488bf0aad2641385fbebc
oo <511543847@qq.com>
2020-03-09 10:09:39 +0800

    命名规则和common

---
+++ src/main/java/com/health/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.health.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/health/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.health.service.impl;
+
+import com.health.service.IUserService;
+
+public class UserServiceImpl implements IUserService {
+}
